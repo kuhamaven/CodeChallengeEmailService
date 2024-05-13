ea45fe891e0a959008b195a74b18338ce15649cc
Kuha <khalil.stessens@ing.austral.edu.ar>
2024-05-13 13:44:27 -0300

    Updated Build.gradle

--- src/main/java/EmailService/EmailServiceApp.java
+++ src/main/java/EmailService/EmailServiceApp.java
@@ -3,22 +3,14 @@ package EmailService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.annotation.PostConstruct;
-import java.io.IOException;
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableSwagger2
 public class EmailServiceApp {
 
     public static void main(String[] args) {
@@ -36,12 +28,4 @@ public class EmailServiceApp {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
 }
